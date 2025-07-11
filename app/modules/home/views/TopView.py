5a8c1f13ccee613f2e9c5f6b4056ef909b7bf53b
kdevy <ykinoshita.cal0@gmail.com>
2025-07-11 10:23:11 +0900

    呪文一覧の表示部分だけ実装

--- app/modules/home/views/TopView.py
+++ app/modules/home/views/TopView.py
@@ -1,12 +1,55 @@
+from collections import defaultdict
 from flask.views import MethodView
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, current_app
+from sqlalchemy import desc, asc
+from app.models import Prompt, PromptGroup
+from app.bootstrap import db
 
 class TopView(MethodView):
     def __init__(self, template):
         self.template = template
 
+    def search(self):
+        query = db.session.query(
+            PromptGroup.id,
+            PromptGroup.name,
+            PromptGroup.updated,
+        ) \
+        .order_by(desc(PromptGroup.updated), desc(PromptGroup.id))
+        prompt_group_list = db.session.execute(query).mappings().all()
+
+        query = db.session.query(
+            Prompt.id,
+            Prompt.prompt_group_id,
+            Prompt.name,
+            Prompt.type,
+            Prompt.is_favorite,
+            Prompt.updated,
+        ) \
+        .order_by(asc(Prompt.type), desc(Prompt.updated), desc(Prompt.id))
+        prompt_list = db.session.execute(query).mappings().all()
+
+        return [prompt_group_list, prompt_list]
+
+    def merge_prompt_group_list(self, prompt_group_list, prompt_list):
+        grouped_prompts = defaultdict(list)
+        for prompt in prompt_list:
+            grouped_prompts[prompt['prompt_group_id']].append(prompt)
+
+        result = []
+        for group in prompt_group_list:
+            dict_group = dict(group)
+            dict_group['prompt'] = grouped_prompts.get(group['id'], [])
+            result.append(dict_group)
+
+        return result
+
     def get(self):
-        context = {}
+        prompt_group_list = self.merge_prompt_group_list(*self.search())
+        context = {
+            "prompt_group_list": prompt_group_list,
+        }
+        current_app.logger.debug(prompt_group_list)
         return render_template(self.template, **context)
 
     def post(self):
